42105483a2e1c75178427e6f34f2863f25376056
Osariemen Daniel Osazee <63324071+kyilax@users.noreply.github.com>
2022-12-23 03:57:29 +0100

    upgrade

---
+++ contracts/HouseV2.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract HouseV2 {
+    uint256 avg;
+
+    function inc() returns () {
+        avg++;
+    }
+
+    function dec() return () {
+        avg--;
+    }
+}
