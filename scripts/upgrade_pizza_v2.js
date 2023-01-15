6494608a171302aa940bf4f47ab757a559075753
Osariemen Daniel Osazee <68135656+thexovc@users.noreply.github.com>
2023-01-15 15:32:38 +0100

    deployed upgrade

--- scripts/upgrade_pizza_v2.js
+++ scripts/upgrade_pizza_v2.js
@@ -1,15 +1,12 @@
-const { ethers, upgrades } = require('hardhat')
+const { ethers, upgrades } = require("hardhat");
 
 const PROXY = "0xde49C10B8CAc7f4F5A975b7e664b79D1fE86345e"
 
 async function main() {
-    const PizzaV2 = ethers.getContractFactory("PizzaV2")
-    console.log("Upgrading Pizza...")
-
-    await upgrades.upgradeProxy(PROXY, PizzaV2)
-    console.log("Pizza  upgraded successfully")
-
-
+    const PizzaV2 = await ethers.getContractFactory("PizzaV2");
+    console.log("Upgrading Pizza...");
+    await upgrades.upgradeProxy(PROXY, PizzaV2);
+    console.log("Pizza upgraded successfully");
 }
 
-main()
+main();
